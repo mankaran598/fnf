d21c3eb0ba1f469cb1c5dc347c61470036db04bd
cyn <cyn0x8+github@gmail.com>
2024-05-09 00:00:59 -0700

    timer sequence
    qol class for a making batches of flxtimers, so you can do stuff like
    ```haxe
    new Sequence([
        {time: 1, callback: some_func},
        {time: 2.5, callback: function() {
            some_var = true;
        }}
    ], Conductor.beatLengthMs / 1000);
    ```

--- source/funkin/util/TimerUtil.hx
+++ source/funkin/util/TimerUtil.hx
@@ -1,5 +1,6 @@
 package funkin.util;
 
+import flixel.util.FlxTimer;
 import funkin.util.tools.FloatTools;
 import haxe.Timer;
 
@@ -45,5 +46,90 @@ class TimerUtil
   {
     var seconds:Float = took(start, end);
     return '${seconds * 1000} ms';
+  }
+}
+
+typedef SequenceEvent =
+{
+  time: Float,
+  callback: ()->Void
+};
+
+class Sequence
+{
+  /**
+   * Create a new timer sequence.
+   * @param events A list of function callbacks along with their corresponding call times, in seconds.
+   * @param mult Optional multiplier for callback times. Great for frame-based or music-based timing.
+   * @param start Whether or not to immediately start the sequence.
+   */
+  public function new(events:Array<SequenceEvent>, mult:Float = 1, start:Bool = true)
+  {
+    for (event in events)
+    {
+      timers.push(new FlxTimer().start(
+        event.time * mult,
+        function(timer:FlxTimer)
+        {
+          event.callback();
+          timers.remove(timer);
+        }
+      ));
+    }
+
+    running = start;
+  }
+
+  /**
+   * The list of uncompleted timers.
+   */
+  private final timers:Array<FlxTimer> = [];
+
+  /**
+   * Controls whether the timers in this sequence are active or not.
+   */
+  public var running(get, set):Bool;
+  private var _running:Bool = true;
+
+  @:noCompletion public function get_running():Bool
+  {
+    return completed ? false : _running;
+  }
+
+  @:noCompletion public function set_running(v:Bool):Bool
+  {
+    for (timer in timers)
+    {
+      timer.active = v;
+    }
+
+    return _running = v;
+  }
+
+  /**
+   * Whether all timers in this sequence have completed or not.
+   */
+  public var completed(get, never);
+  
+  @:noCompletion public function get_completed():Bool
+  {
+    return timers.length == 0;
+  }
+
+  /**
+   * Clean up and destroy this sequence.
+   */
+  public function destroy():Void
+  {
+    for (timer in timers)
+    {
+      timer.cancel();
+      timer.destroy();
+    }
+
+    while (!completed)
+    {
+      timers.pop();
+    }
   }
 }
