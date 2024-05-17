5ea45b4ef7985f9b800a16be1f38f2c852aa367e
Ralty <78720179+Raltyro@users.noreply.github.com>
2024-05-17 11:09:49 +0700

    Fix an oversight on Bopper.correctAnimationName

--- source/funkin/play/stage/Bopper.hx
+++ source/funkin/play/stage/Bopper.hx
@@ -234,35 +234,22 @@ class Bopper extends StageProp implements IPlayStateScriptedClass
     // If the animation exists, we're good.
     if (hasAnimation(name)) return name;
 
-    FlxG.log.notice('Bopper tried to play animation "$name" that does not exist, stripping suffixes...');
-
     // Attempt to strip a `-alt` suffix, if it exists.
-    if (name.lastIndexOf('-') != -1)
+    var idx = name.lastIndexOf('-');
+    if (idx != -1)
     {
-      var correctName = name.substring(0, name.lastIndexOf('-'));
       FlxG.log.notice('Bopper tried to play animation "$name" that does not exist, stripping suffixes...');
-      return correctAnimationName(correctName);
+      return correctAnimationName(name.substr(0, idx), fallback);
+    }
+    else if (fallback != null && fallback != name)
+    {
+      FlxG.log.warn('Bopper tried to play animation "$name" that does not exist, fallback to $fallback');
+      return correctAnimationName(fallback);
     }
     else
     {
-      if (fallback != null)
-      {
-        if (fallback == name)
-        {
-          FlxG.log.error('Bopper tried to play animation "$name" that does not exist! This is bad!');
-          return null;
-        }
-        else
-        {
-          FlxG.log.warn('Bopper tried to play animation "$name" that does not exist, fallback to idle...');
-          return correctAnimationName('idle');
-        }
-      }
-      else
-      {
-        FlxG.log.error('Bopper tried to play animation "$name" that does not exist! This is bad!');
-        return null;
-      }
+      FlxG.log.error('Bopper tried to play animation "$name" that does not exist! This is bad!');
+      return null;
     }
   }
 
